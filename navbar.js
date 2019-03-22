5f2d383213dba58612c6c941a0c2cfc146818189
Achwatm <43848956+Achwatm@users.noreply.github.com>
2019-03-22 16:00:12 +0100

    Add files via upload

---
+++ navbar.js
@@ -0,0 +1,5 @@
+let nav = '<nav><a href="index.html">Home</a>'+
+    '<a href="kontakt.html">Kontakt</a></nav>';
+let navBar = document.querySelector(".nav-bar");
+navBar.innerHTML = nav;
+
---
+++ script.js
@@ -0,0 +1,9 @@
+let btn = document.querySelector('.button');
+let box = document.querySelector('.box');
+let div = '<div style=" background-color: blue;"></div>';
+btn.addEventListener('click',function () {
+    box.style.backgroundColor='red';
+});
+
+
+
